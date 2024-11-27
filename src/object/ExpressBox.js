3c82f7e1cd0ed5156eea36a7aa32526a09f4a024
孙培伶 <3061784569@qq.com>
2024-11-27 10:21:19 +0800

    p

--- src/object/ExpressBox.js
+++ src/object/ExpressBox.js
@@ -9,6 +9,18 @@ import express5 from '../res/维纳斯/1.png';
 import express6 from '../res/维纳斯/3.png';
 import express7 from '../res/维纳斯/4.png';
 import express8 from '../res/维纳斯/5.png';
+import express9 from '../res/毕加索/1.png';
+import express10 from '../res/毕加索/2.png';
+import express11 from '../res/毕加索/3.png';
+import express12 from '../res/毕加索/4.png';
+import express13 from '../res/梵高/1.png';
+import express14 from '../res/梵高/2.png';
+import express15 from '../res/梵高/3.png';
+import express16 from '../res/梵高/4.png';
+import express17 from '../res/野兽派/1.png';
+import express18 from '../res/野兽派/2.png';
+import express19 from '../res/野兽派/3.png';
+import express20 from '../res/野兽派/4.png';
 export default class ExpressBox extends Box {
   constructor(prev) {
     super(prev)
@@ -32,7 +44,7 @@ export default class ExpressBox extends Box {
   }
 
   getRandomExpress() {
-    const expressImages = [express1, express2, express3, express4, express5, express6, express7, express8];
+    const expressImages = [express1, express2, express3, express4, express5, express6, express7, express8,express9,express10,express11,express12,express13,express14,express15,express16,express17,express18,express19,express20];
     
     // 洗牌算法
     for (let i = expressImages.length - 1; i > 0; i--) {
